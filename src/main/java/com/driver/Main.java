751931787264fc28f9d3bc704b098c92670be253
RushilShandilya <rushilcujoh@gmail.com>
2024-01-12 22:05:56 +0530

    Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly rwonly = new RWOnly();
+      //rwonly.name = "Rushil";
+      //Facing an error - name' has private access in 'com.driver.RWOnly';
+
+      rwonly.setName("Kageyama");
+      System.out.println(rwonly.getName());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    String getName(){
+        return name;
+    }
+    void setName(String _name){
+        name = _name;
+    }
 }
